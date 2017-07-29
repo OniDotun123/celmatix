ed44962674ed8451be09eba25519cc0fae6e7db3
Adedotun <onidotun99@gmail.com>
2017-07-29 18:54:24 -0400

    install rails admin. using devise for auth purpose

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,6 @@
 Rails.application.routes.draw do
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "home#index"
-
-  devise_for :users, controllers: {
-      sessions: 'users/sessions',
-      passwords: 'users/passwords',
-      registrations: 'users/registrations'
-  }
-end
+end 
